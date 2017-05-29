21087703e8293cf68e4c6a6aabb8c5710dfe28f2
Damien Lespiau <damien.lespiau@intel.com>
2017-05-29 16:09:30 +0100

    pkg/cover: Sweep the hacky bits under the rug.
    Well, in a package really.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- examples/calc/main_test.go
+++ examples/calc/main_test.go
@@ -17,13 +17,11 @@
 package main
 
 import (
-	"flag"
-	"io"
 	"os"
 	"path"
-	"strings"
 	"testing"
 
+	"github.com/dlespiau/covertool/pkg/cover"
 	"github.com/dlespiau/covertool/pkg/exit"
 )
 
@@ -46,50 +44,12 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-type dummyTestDeps struct{}
-
-func (d dummyTestDeps) MatchString(pat, str string) (bool, error)   { return false, nil }
-func (d dummyTestDeps) StartCPUProfile(io.Writer) error             { return nil }
-func (d dummyTestDeps) StopCPUProfile()                             {}
-func (d dummyTestDeps) WriteHeapProfile(io.Writer) error            { return nil }
-func (d dummyTestDeps) WriteProfileTo(string, io.Writer, int) error { return nil }
-
-// FlushProfiles flushes test profiles to disk. It works by build and executing
-// a dummy list of 1 test. This is to ensure we execute the M.after() function
-// (a function internal to the testing package) where all reporting (cpu, mem,
-// cover, ... profiles) is flushed to disk.
-func FlushProfiles() {
-	// We define a dummy test function so we don't get the "there's no test
-	// defined!" warning from the testing package.
-	tests := []testing.InternalTest{
-		testing.InternalTest{"TestDummy", func(*testing.T) {}},
-	}
-	benchmarks := []testing.InternalBenchmark{}
-	examples := []testing.InternalExample{}
-	dummyM := testing.MainStart(dummyTestDeps{}, tests, benchmarks, examples)
-	dummyM.Run()
-}
-
 func TestMain(m *testing.M) {
-	// Parse the command line using the stdlib flag package so the flags defined
-	// in the testing package get populated.
-	flag.Parse()
-
-	// Strip command line arguments that were for the testing package and are now
-	// handled.
-	var runtimeArgs []string
-	for _, arg := range os.Args {
-		if strings.HasPrefix(arg, "-test.") ||
-			strings.HasPrefix(arg, "-httptest.") {
-			continue
-		}
-		runtimeArgs = append(runtimeArgs, arg)
-	}
-	os.Args = runtimeArgs
+	cover.ParseAndStripTestFlags()
 
 	// Make sure we have the opportunity to flush the coverage report to disk when
 	// terminating the process.
-	exit.Atexit(FlushProfiles)
+	exit.Atexit(cover.FlushProfiles)
 
 	// If the test binary name is cc-runtime.coverage, we've are being asked to
 	// run the coverage-instrumented cc-runtime.
